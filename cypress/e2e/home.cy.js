b4b78a8a65b9b53ab627ddede19cf15edf0d43b6
Irwing Naranjo <khalisser@gmail.com>
2023-10-05 15:09:52 -0300

    feat: agregar precommit y prepush

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+describe('test home', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('h1').should('contain', 'OpenFeedBack')
+  })
+})
