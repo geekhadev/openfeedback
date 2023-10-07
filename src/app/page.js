b2aad00ad7efbbca303cc0428da53d2f6b04c0ac
Irwing Naranjo <khalisser@gmail.com>
2023-10-07 10:31:12 -0300

    refactor: cambiamos el h1 por h2

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>OpenFeedBack</h1>
+      <h2>OpenFeedBack</h2>
     </main>
   )
 }
