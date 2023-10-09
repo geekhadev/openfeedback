2a3ae71955ddcde1c2600ac16f0e0cedd10c4e15
Daniel9822 <dhdany64@gmail.com>
2023-10-09 13:35:46 -0400

    refactor: se creo una carpeta constant donde se alojaran los strings de la home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
+import { login, projects, register, title } from '@/constant/home'
+
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between py-6 items-center">
-      <h1 className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-        OpenFeedback
+    <header className='flex flex-row justify-between py-6 items-center'>
+      <h1 className='font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+        {title}
       </h1>
-      <nav className="gap-4 flex text-xl">
-        <a className="font-bold hover:text-pink-400" href="/#projects">Proyectos</a>
-        <a className="font-bold hover:text-pink-400" href="#">Login</a>
-        <a className="font-bold hover:text-pink-400" href="#">Register</a>
+      <nav className='gap-4 flex text-xl'>
+        <a className='font-bold hover:text-pink-400' href='/#projects'>
+          {projects}
+        </a>
+        <a className='font-bold hover:text-pink-400' href='#'>
+          {login}
+        </a>
+        <a className='font-bold hover:text-pink-400' href='#'>
+          {register}
+        </a>
       </nav>
     </header>
   )
