301ae85e38d46e1770a98bf6a438261be6eb2daa
Lion <leonardofontan14@gmail.com>
2023-10-11 17:17:24 -0300

    refactor: make Header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
-import { login, projects, register, title } from '@/constant/home'
+import { login, projects, register, title } from "@/constant/home";
 
 const Header = () => {
   return (
-    <header className='flex flex-row justify-between py-6 items-center'>
-      <h1 className='font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+    <header className="flex flex-col md:flex-row md:px-6 gap-1 justify-between py-6 items-center">
+      <h1 className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         {title}
       </h1>
-      <nav className='gap-4 flex text-xl'>
-        <a className='font-bold hover:text-pink-400' href='/#projects'>
+      <nav className="gap-4 flex text-xl">
+        <a className="font-bold hover:text-pink-400" href="/#projects">
           {projects}
         </a>
-        <a className='font-bold hover:text-pink-400' href='#'>
+        <a className="font-bold hover:text-pink-400" href="#">
           {login}
         </a>
-        <a className='font-bold hover:text-pink-400' href='#'>
+        <a className="font-bold hover:text-pink-400" href="#">
           {register}
         </a>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
