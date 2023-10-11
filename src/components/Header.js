fd43bbd3718bb6ce5afef73c90d27a9b9ffb127e
Adrián Sanz <adriybeto@hotmail.com>
2023-10-11 23:14:57 +0200

    refactor HTML structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,31 @@
+"use client";
 import { login, projects, register, title } from '@/constant/home'
+import { usePathname } from 'next/navigation'
+import Hero from '@/components/Hero'
 
 const Header = () => {
+  const path = usePathname();
+  
+  const isRootPage = path === "/" ;
+  return (
+    (isRootPage ? 
+      <div className="bg-[url('/images/bg.png')] bg-cover relative h-screen">
+          <div className="absolute bg-gradient-to-r to-gray-900 from-black opacity-80 w-full h-full z-0"></div>
+          <div className="container m-auto z-10 relative h-full flex flex-col max-w-4xl">
+            <HeaderNavar />
+            <Hero />
+          </div>
+        </div> 
+      : 
+      <HeaderNavar />
+    )
+  )
+}
+
+export default Header
+
+
+function HeaderNavar() {
   return (
     <header className='flex flex-row justify-between py-6 items-center'>
       <h1 className='font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
@@ -20,5 +45,3 @@ const Header = () => {
     </header>
   )
 }
-
-export default Header
