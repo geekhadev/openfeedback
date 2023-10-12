4e6d3d5e64f44a32cc4c5510ff8a7fefbfaaeefc
Adrián Sanz <adriybeto@hotmail.com>
2023-10-12 15:48:49 +0200

    lint fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,11 +14,9 @@ export default function RootLayout ({ children }) {
   return (
     <html lang="es">
       <body className={inter.className}>
-      
         <Header />
         {children}
         <Footer />
-        
       </body>
     </html>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,41 @@
-"use client";
+'use client'
 import { login, projects, register, title } from '@/constant/home'
 import { usePathname } from 'next/navigation'
 import Hero from '@/components/Hero'
 
 const Header = () => {
-  const path = usePathname();
-  
-  const isRootPage = path === "/" ;
+  const path = usePathname()
+  const isRootPage = path === '/'
   return (
-    (isRootPage ? 
-      <div className="bg-[url('/images/bg.png')] bg-cover relative h-screen">
-          <div className="absolute bg-gradient-to-r to-gray-900 from-black opacity-80 w-full h-full z-0"></div>
-          <div className="container m-auto z-10 relative h-full flex flex-col max-w-4xl">
-            <HeaderNavar />
-            <Hero />
-          </div>
-        </div> 
-      : 
-      <HeaderNavar />
+    (isRootPage
+      ? <div className="bg-[url('/images/bg.png')] bg-cover relative h-screen">
+        <div className="absolute bg-gradient-to-r to-gray-900 from-black opacity-80 w-full h-full z-0"></div>
+        <div className="container m-auto z-10 relative h-full flex flex-col max-w-4xl">
+          <HeaderNavar />
+          <Hero />
+        </div>
+      </div>
+      : <HeaderNavar />
     )
   )
 }
 
 export default Header
 
-
-function HeaderNavar() {
+function HeaderNavar () {
   return (
-    <header className='flex flex-row justify-between py-6 items-center'>
-      <h1 className='font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+    <header className="flex flex-row justify-between py-6 items-center">
+      <h1 className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         {title}
       </h1>
-      <nav className='gap-4 flex text-xl'>
-        <a className='font-bold hover:text-pink-400' href='/#projects'>
+      <nav className="gap-4 flex text-xl">
+        <a className="font-bold hover:text-pink-400" href='/#projects'>
           {projects}
         </a>
-        <a className='font-bold hover:text-pink-400' href='#'>
+        <a className="font-bold hover:text-pink-400" href='#'>
           {login}
         </a>
-        <a className='font-bold hover:text-pink-400' href='#'>
+        <a className="font-bold hover:text-pink-400" href='#'>
           {register}
         </a>
       </nav>
