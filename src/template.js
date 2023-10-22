db7ce4306fbb16ca70c146023b03d723fde7cca3
Daniel9822 <dhdany64@gmail.com>
2023-10-21 23:52:44 -0400

    refactor: movi el tamplate de project y agregue algunas imagenes para tener mejor visualizacion

---
+++ src/template.js
@@ -0,0 +1,75 @@
+// Solo es para simular alguna data las
+// imagenes las saque de un projecto personal 😂
+
+export const PROJECTS = [
+  {
+    author: 'Daniel H',
+    id: '8379b07c-5e9b-4d77-a9ae-466fdce6715b',
+    title: 'Website Somos Juniors',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%204eed1aa1-3053-42db-bbd9-3fdd9f46d55b?alt=media&token=5645736b-3924-43aa-9c03-30565b6e3c84&_gl=1*1ucv576*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQzNDcuMjcuMC4w'
+      },
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%203e267061-f8c7-4b95-829f-2c201c6e971f?alt=media&token=8941c442-1e07-4f0d-93fd-b103867e992f&_gl=1*z0y8c5*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQwMzguMjguMC4w'
+      }
+    ],
+    categories: ['Astro', 'React', 'Tailwind'],
+    rating: '★★★★★',
+    createdAt: '23/10/22',
+    updatedAt: '23/10/22'
+  },
+  {
+    author: 'Carolina J',
+    id: '8379b07c-5e9b-4d77-a9ae-466fdce6715c',
+    title: 'API con NodeJS',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%204eed1aa1-3053-42db-bbd9-3fdd9f46d55b?alt=media&token=5645736b-3924-43aa-9c03-30565b6e3c84&_gl=1*1ucv576*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQzNDcuMjcuMC4w'
+      },
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%2004ffdd6e-432b-4c57-9878-bc882ee3d342?alt=media&token=df73407c-7ac3-478c-b5f7-8a53d4b7de95'
+      }
+    ],
+    categories: ['nodejs', 'express', 'mongodb'],
+    rating: '★★★★',
+    createdAt: '23/10/22',
+    updatedAt: '23/10/22'
+  },
+  {
+    author: 'Carmen C',
+    id: '8379b07c-5e9b-4d77-a9ae-466fdce6715d',
+    title: 'API con NestJS',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%203e267061-f8c7-4b95-829f-2c201c6e971f?alt=media&token=8941c442-1e07-4f0d-93fd-b103867e992f&_gl=1*z0y8c5*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQwMzguMjguMC4w'
+      }
+    ],
+    categories: ['nestjs', 'express', 'mongodb'],
+    rating: '★★★★★',
+    createdAt: '23/10/22',
+    updatedAt: '23/10/22'
+  },
+  {
+    author: 'Alexandra MVP',
+    id: '8379b07c-5e9b-4d77-a9ae-466fdce6715e',
+    title: 'API con Laravel',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%2041db5d2c-73a3-4b1f-9e29-61f1b1a380a1?alt=media&token=e7bfb6a0-9175-47c2-808d-e374d3577a25&_gl=1*12p8kwk*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQyNTYuNTUuMC4w'
+      }
+    ],
+    categories: ['laravel', 'php', 'mysql'],
+    rating: '★★★',
+    createdAt: '23/10/22',
+    updatedAt: '23/10/22'
+  }
+]
