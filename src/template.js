8a55857c07a687abd649245d4788f038bf7b354a
Daniel9822 <dhdany64@gmail.com>
2023-10-22 00:32:23 -0400

    fix: puse la imagen que estaba

--- src/template.js
+++ src/template.js
@@ -1,5 +1,4 @@
-// Solo es para simular alguna data las
-// imagenes las saque de un projecto personal 😂
+// Solo es para simular alguna data
 
 export const PROJECTS = [
   {
@@ -10,10 +9,10 @@ export const PROJECTS = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
     images: [
       {
-        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%204eed1aa1-3053-42db-bbd9-3fdd9f46d55b?alt=media&token=5645736b-3924-43aa-9c03-30565b6e3c84&_gl=1*1ucv576*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQzNDcuMjcuMC4w'
+        url: 'https://placehold.co/600x400'
       },
       {
-        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%203e267061-f8c7-4b95-829f-2c201c6e971f?alt=media&token=8941c442-1e07-4f0d-93fd-b103867e992f&_gl=1*z0y8c5*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQwMzguMjguMC4w'
+        url: 'https://placehold.co/600x400'
       }
     ],
     categories: ['Astro', 'React', 'Tailwind'],
@@ -29,10 +28,10 @@ export const PROJECTS = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
     images: [
       {
-        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%204eed1aa1-3053-42db-bbd9-3fdd9f46d55b?alt=media&token=5645736b-3924-43aa-9c03-30565b6e3c84&_gl=1*1ucv576*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQzNDcuMjcuMC4w'
+        url: 'https://placehold.co/600x400'
       },
       {
-        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%2004ffdd6e-432b-4c57-9878-bc882ee3d342?alt=media&token=df73407c-7ac3-478c-b5f7-8a53d4b7de95'
+        url: 'https://placehold.co/600x400'
       }
     ],
     categories: ['nodejs', 'express', 'mongodb'],
@@ -48,7 +47,7 @@ export const PROJECTS = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
     images: [
       {
-        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%203e267061-f8c7-4b95-829f-2c201c6e971f?alt=media&token=8941c442-1e07-4f0d-93fd-b103867e992f&_gl=1*z0y8c5*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQwMzguMjguMC4w'
+        url: 'https://placehold.co/600x400'
       }
     ],
     categories: ['nestjs', 'express', 'mongodb'],
@@ -64,7 +63,7 @@ export const PROJECTS = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
     images: [
       {
-        url: 'https://firebasestorage.googleapis.com/v0/b/henry-social-884d1.appspot.com/o/henry-social-post%2F%2041db5d2c-73a3-4b1f-9e29-61f1b1a380a1?alt=media&token=e7bfb6a0-9175-47c2-808d-e374d3577a25&_gl=1*12p8kwk*_ga*MTk3ODk4OTA4Mi4xNjg2Njk5ODk3*_ga_CW55HF8NVT*MTY5Nzk0Mzk0NS40Ny4xLjE2OTc5NDQyNTYuNTUuMC4w'
+        url: 'https://placehold.co/600x400'
       }
     ],
     categories: ['laravel', 'php', 'mysql'],
