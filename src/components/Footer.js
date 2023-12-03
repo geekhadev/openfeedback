add0b3c8251bf0d2e2100f0ea178b301830b9d30
Mateo Algañarás <tutealga@gmail.com>
2023-12-03 20:50:46 -0300

    fix: centrado de los creditos del footer y espacios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,8 @@ import { paragraphFooter, titleLinkFooter } from '@/constant/home'
 
 const Footer = () => {
   return (
-    <footer className='py-12'>
-      <p>
-        {paragraphFooter} -
-        <a href='#' className='text-blue-400'>
-          {titleLinkFooter}
-        </a>
+    <footer className='flex justify-center py-12'>
+      <p>{paragraphFooter} - <a href='#' className='text-blue-400'>{titleLinkFooter}</a>
       </p>
     </footer>
   )
